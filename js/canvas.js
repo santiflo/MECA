b511a58fba93dc174a8d4fa300ec7cbe16b56bd9
Dilan Madhava Chávez Molano <dilanmolano@gmail.com>
2021-02-08 20:47:14 -0500

    Better code

--- js/canvas.js
+++ js/canvas.js
@@ -1,3 +1,28 @@
+// Display custom canvas. In this case it's a blue, 5 pixel 
+// border that resizes along with the browser window.
+function redraw(context) {
+    context.strokeStyle = 'red';
+    context.lineWidth = '5';
+    context.strokeRect(0, 0, window.innerWidth, window.innerHeight);
+}
+
+// Runs each time the DOM window resize event fires.
+// Resets the canvas dimensions to match window,
+// then draws the new borders accordingly.
+function resizeCanvas(htmlCanvas, context) {
+    htmlCanvas.width = window.innerWidth;
+    htmlCanvas.height = window.innerHeight;
+    redraw(context);
+}
+
+function initialize(htmlCanvas, context) {
+    // Register an event listener to call the resizeCanvas() function 
+    // each time the window is resized.
+    window.addEventListener('resize', resizeCanvas, false);
+    // Draw canvas border for the first time.
+    resizeCanvas(htmlCanvas, context);
+}
+
 function main() {
     var
         // Obtain a reference to the canvas element using its id.
@@ -6,32 +31,7 @@ function main() {
         context = htmlCanvas.getContext('2d');
 
     // Start listening to resize events and draw canvas.
-    initialize();
-
-    function initialize() {
-        // Register an event listener to call the resizeCanvas() function 
-        // each time the window is resized.
-        window.addEventListener('resize', resizeCanvas, false);
-        // Draw canvas border for the first time.
-        resizeCanvas();
-    }
-
-    // Display custom canvas. In this case it's a blue, 5 pixel 
-    // border that resizes along with the browser window.
-    function redraw() {
-        context.strokeStyle = 'blue';
-        context.lineWidth = '5';
-        context.strokeRect(0, 0, window.innerWidth, window.innerHeight);
-    }
-
-    // Runs each time the DOM window resize event fires.
-    // Resets the canvas dimensions to match window,
-    // then draws the new borders accordingly.
-    function resizeCanvas() {
-        htmlCanvas.width = window.innerWidth;
-        htmlCanvas.height = window.innerHeight;
-        redraw();
-    }
+    initialize(htmlCanvas, context);
 }
 
 main();
