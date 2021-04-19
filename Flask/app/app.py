b75b2b71dd205a31222f1a38b65703fc84ac361f
santiflo <santiflo@javerianacali.edu.co>
2021-04-19 11:28:53 -0500

    Update app.py

--- Flask/app/app.py
+++ Flask/app/app.py
@@ -22,35 +22,8 @@ from app.Models import Model_Types
 from app.Models import Model_Categories
 
 #Controllers
+from app.Controllers import Controller_errors
 from app.Controllers import Controller_Users
 from app.Controllers import Controller_Types
 from app.Controllers import Controller_Documentaries
-#from app.Controllers import Controller_Multimedia
-
-"""
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
-
-@app.route('/string/')
-def return_string():
-    return 'Hello, world!'	
-
-@app.route('/object/')
-def return_object():
-    headers = {'Content-Type': 'text/plain'}
-    return make_response('Hello, world!', 200,headers)	
-
-@app.route('/tuple/')
-def return_tuple():
-    return 'Hello, world!', 200, {'Content-Type':'text/plain'}
-
-@app.route('/login')
-def login():
-	abort(401)
-	# Esta línea no se ejecuta
-
-@app.errorhandler(404)
-def page_not_found(error):
-    return 'Página no encontrada...', 404
-"""
+from app.Controllers import Controller_Categories
