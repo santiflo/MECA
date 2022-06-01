711c77ed07f795fd0be1e0634637f328d36c5c6c
santiflo <santiflo@javerianacali.edu.co>
2022-05-31 22:51:11 -0500

    Changes on de excuter

--- index.py
+++ index.py
@@ -1,11 +1,9 @@
-# -*- coding: utf-8 -*-
-
 import os
 from app.app import app, db
 
 # Para Heroku
 port = os.environ["PORT"]
-#db.create_all()
+db.create_all()
 app.run(debug=True , host="0.0.0.0", port = int(port))
 # Para local
 #app.run(debug=True , host="0.0.0.0")
--- local_index.py
+++ local_index.py
@@ -1,10 +1,9 @@
-# -*- coding: utf-8 -*-
-
-#import os
+import os
 from app.app import app, db
 
 # Para Heroku
-#port = os.environ["PORT"]
+port = os.environ["PORT"]
+db.create_all()
 #app.run(debug=True , host="0.0.0.0", port = int(port))
 # Para local
 app.run(debug=True , host="0.0.0.0")
